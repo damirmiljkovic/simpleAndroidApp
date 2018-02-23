f00af55bd93ed3436c9123c6f955cfd8c6bad623
Damir Miljkovic <damirmi@gmail.com>
2018-02-23 14:28:12 +0100

    added empty activity and fragments

---
+++ app/src/main/java/net/idevcorp/simpleandroidapp/ui/activities/InitialActivity.java
@@ -0,0 +1,8 @@
+package net.idevcorp.simpleandroidapp.ui.activities;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+public class InitialActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/net/idevcorp/simpleandroidapp/ui/fragments/WelcomFragment.java
@@ -0,0 +1,6 @@
+package net.idevcorp.simpleandroidapp.ui.fragments;
+
+
+public class WelcomFragment {
+
+}
